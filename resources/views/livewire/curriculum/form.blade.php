30619960c7b30c4c68c1820fb522b68a6d1483fb
Matheus Delazeri <matheusdelazeris@gmail.com>
2024-06-17 22:36:49 -0300

    - Feat: added pt_BR translation

--- resources/views/livewire/curriculum/form.blade.php
+++ resources/views/livewire/curriculum/form.blade.php
@@ -8,7 +8,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"/>
                 </svg>
-                Back
+                {{ __('Back') }}
             </x-secondary-button>
             @if(is_null($curriculumId))
                 <x-primary-button type="submit">{{ __('Save') }}</x-primary-button>
