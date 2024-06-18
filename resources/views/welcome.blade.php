d8def475305bc112333ced3f0b5baa10ba379fc3
Matheus Delazeri <matheusdelazeris@gmail.com>
2024-06-18 10:17:55 -0300

    - UX: added content to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,64 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Curriculum Maker') }}</title>
-        <link rel="icon" href="{{ asset('favicon.svg') }}" sizes="any" type="image/svg+xml">
+    <title>{{ config('app.name', 'Curriculum Maker') }}</title>
+    <link rel="icon" href="{{ asset('favicon.svg') }}" sizes="any" type="image/svg+xml">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
 
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased font-sans">
-        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            <div class="relative min-h-screen flex flex-col items-center selection:bg-[#FF2D20] selection:text-white">
-                <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-center lg:col-start-2">
-                            <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.03v13m0-13c-2.819-.831-4.715-1.076-8.029-1.023A.99.99 0 0 0 3 6v11c0 .563.466 1.014 1.03 1.007 3.122-.043 5.018.212 7.97 1.023m0-13c2.819-.831 4.715-1.076 8.029-1.023A.99.99 0 0 1 21 6v11c0 .563-.466 1.014-1.03 1.007-3.122-.043-5.018.212-7.97 1.023"/>
-                            </svg>
-                        </div>
-                        @if (Route::has('login'))
-                            <livewire:welcome.navigation />
-                        @endif
-                    </header>
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased font-sans">
+<div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+    <div class="relative min-h-screen flex flex-col items-center selection:bg-[#FF2D20] selection:text-white">
+        <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+            <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                <div class="flex lg:justify-center lg:col-start-2">
+                    <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 6.03v13m0-13c-2.819-.831-4.715-1.076-8.029-1.023A.99.99 0 0 0 3 6v11c0 .563.466 1.014 1.03 1.007 3.122-.043 5.018.212 7.97 1.023m0-13c2.819-.831 4.715-1.076 8.029-1.023A.99.99 0 0 1 21 6v11c0 .563-.466 1.014-1.03 1.007-3.122-.043-5.018.212-7.97 1.023"/>
+                    </svg>
+                </div>
+                @if (Route::has('login'))
+                    <livewire:welcome.navigation/>
+                @endif
+            </header>
 
-                    <main class="mt-6">
-                        <div class="grid gap-6 lg:grid-cols-1 lg:gap-8">
-                            <div
-                                id="docs-card"
-                                class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                            >
-                                <div class="relative flex items-center gap-6 lg:items-end">
-                                    <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
-                                        <div class="pt-3 sm:pt-5 lg:pt-0">
-                                            <h2 class="text-xl font-semibold text-black dark:text-white">Lorem ipsum</h2>
+            <main class="mt-6">
+                <div class="grid gap-6 lg:grid-cols-1 lg:gap-8">
+                    <div
+                        id="docs-card"
+                        class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                    >
+                        <div class="relative flex items-center gap-6 lg:items-end">
+                            <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
+                                <div class="pt-3 sm:pt-5 lg:pt-0">
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">{{ __('Welcome to Curriculum Maker') }}</h2>
 
-                                            <p class="mt-4 text-sm/relaxed">
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim in nisl ut hendrerit. Etiam nec semper nulla, in bibendum lorem. Suspendisse sed est ante. Duis malesuada, massa eget blandit rutrum, orci nibh blandit metus, vel pretium felis nisl ut erat. Curabitur scelerisque quam in pharetra consectetur. Praesent et orci porta, cursus arcu sit amet, ornare tellus. Nunc vel justo velit. Integer eu ligula ac ipsum viverra fermentum. Praesent lacinia dui vel erat elementum faucibus. Vivamus facilisis magna suscipit ligula rutrum ultrices. Integer sed sagittis risus.
-                                            </p>
-                                        </div>
+                                    <p class="mt-4 text-sm/relaxed">
+                                        {{ __('Create and customize your resume quickly and easily. Fill in your basic information, and our team of reviewers and builders will take care of the rest for you. Start now and have a professional resume ready in no time!') }}
+                                    </p>
+
+                                    <div class="mt-6">
+                                        <a href="{{ route('register') }}" class="text-black font-bold hover:underline">{{ __('Create your account') }}</a>
+                                        <span class="mx-2">{{ __('or') }}</span>
+                                        <a href="{{ route('login') }}" class="text-black font-bold hover:underline">{{ __('Log in') }}</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </main>
+                    </div>
                 </div>
-            </div>
+            </main>
         </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
